2f6baab1bdab6aef327fb80a9d26b4c8ff281aaa
Max96312 <k0mp4ny16@gmail.com>
2024-05-29 19:38:23 +0900

    refactor: 폴더 구조 개선, init: add springmvc-project

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+//@Controller
+//public class OldController {
+//
+//    @RequestMapping("/springmvc/v1/members/new-form")
+//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        System.out.println("OldController.handleRequest");
+//        return new ModelAndView("new-form");
+//    }
+//}
