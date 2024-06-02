909a45b33b2046c29654185bdcda4078310fa410
Max96312 <k0mp4ny16@gmail.com>
2024-06-03 02:39:00 +0900

    sec05 mvc structure

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -2,17 +2,15 @@ package hello.servlet.web.springmvc.old;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
-
-//@Controller
-//public class OldController {
-//
-//    @RequestMapping("/springmvc/v1/members/new-form")
-//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//        System.out.println("OldController.handleRequest");
-//        return new ModelAndView("new-form");
-//    }
-//}
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
